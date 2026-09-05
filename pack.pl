1b574a7e6e8db4ccc6ad7c57c041d1baba4ecbdd
Bikal Gurung <gbikal@gmail.com>
2026-09-05 10:03:04 +0100

    fix: pack naming convention

--- pack.pl
+++ pack.pl
@@ -2,6 +2,6 @@ name(http_date).
 title('RFC 9110 HTTP date parsing and formatting').
 version('0.1.0').
 author('Bikal Gurung', 'gbikal@gmail.com').
-home('https://github.com/bikallem/http-date-pl').
-download('https://github.com/bikallem/http-date-pl/archive/v0.1.0.zip').
+home('https://github.com/bikallem/http_date').
+download('https://github.com/bikallem/http_date/archive/v0.1.0.zip').
 
